74423f8a88f2bc99520b15745ae71ef4b98d076f
DionataNunesGarcia <dionata.nunes.garcia@gmail.com>
2023-10-19 18:50:43 -0300

    add new theme and configs

---
+++ web/themes/custom/kilombo_theme/js/kilombo-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Kilombo behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.kilomboTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
